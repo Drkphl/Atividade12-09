362947e04f7f803b4373dd727bab192338655f81
Drkphl <leonardotizon@hotmail.com>
2024-09-12 20:13:45 -0300

    adicionado db/app/middle

---
+++ LivroRegex/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const livro_routes = require('./routes/livro.js');
+const autor_routes = require('./routes/autor.js');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/livro', livro_routes);
+app.use('/autor', autor_routes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
